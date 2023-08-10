efac3bd43146aa9066bb06fdb238f9be76f383f7
iampawan <pawan221b@gmail.com>
2023-08-10 12:42:56 +0530

    Fixes

--- packages/frontend/test/unit_test.dart
+++ packages/frontend/test/unit_test.dart
@@ -2,7 +2,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:frontend/core/data/remote_task_repo.dart';
 
 void main() {
-  test('Counter increments smoke test', () async {
+  test('Fetch All Tasks API Test', () async {
     final response = await RemoteTaskRepo().fetchAllTasks();
     expect(response.length, 3);
   });
